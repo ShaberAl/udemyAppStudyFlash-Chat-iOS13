9d9c77684fe55c5d90cf1fa731cfa3c7dd1a1ab7
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-26 16:41:40 +0500

    Ready

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by MacOS on 26.10.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
